034445608938b766e299eb3718f2a61284b55cd5
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2020-06-27 16:20:40 -0700

    Initial commit with Unity project files.

---
+++ FlowField/Assets/Scripts/TestScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("HelloWorld:");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
