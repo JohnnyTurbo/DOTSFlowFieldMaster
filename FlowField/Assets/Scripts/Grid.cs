2d5da705a273c2717efa59128274ffcb43bcae1a
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2020-06-27 17:50:09 -0700

    Implemented Grid and Node classes.

---
+++ FlowField/Assets/Scripts/Grid.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TMG.FlowField
+{
+	public class Grid : MonoBehaviour
+	{
+		public Vector2Int gridSize;
+		public float nodeRadius = 1;
+
+		Node[,] grid;
+		float nodeDiameter;
+
+		private void Start()
+		{
+			nodeDiameter = nodeRadius * 2;
+			CreateGrid();
+		}
+
+		private void CreateGrid()
+		{
+			Debug.Log("Creating Cube");
+			grid = new Node[gridSize.x, gridSize.y];
+
+			for(int x = 0; x < gridSize.x; x++)
+			{
+				for(int y = 0; y < gridSize.y; y++)
+				{
+					Vector3 worldPos = new Vector3(nodeDiameter * x, 0, nodeDiameter * y);
+					grid[x, y] = new Node(true, worldPos);
+				}
+			}
+		}
+
+		void OnDrawGizmos()
+		{
+			
+			if(grid != null)
+			{
+				
+				foreach (Node n in grid)
+				{
+					Gizmos.color = n.walkable ? Color.green : Color.red;
+					Gizmos.DrawCube(n.worldPos, Vector3.one * (nodeDiameter - 0.1f));
+				}
+			}
+		}
+	}
+}
---
+++ FlowField/Assets/Scripts/Node.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Node
+{
+	public bool walkable;
+	public Vector3 worldPos;
+	
+	public Node(bool _walkable, Vector3 _worldPos)
+	{
+		walkable = _walkable;
+		worldPos = _worldPos;
+	}
+}
