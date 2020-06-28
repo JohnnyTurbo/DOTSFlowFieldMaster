b7207faa9fac8ca2f2f9ebaa37cdb5c7630bc664
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2020-06-27 18:52:01 -0700

    Added ability to make nodes impassible using Ctrl + Click.

--- FlowField/Assets/Scripts/Grid.cs
+++ FlowField/Assets/Scripts/Grid.cs
@@ -19,6 +19,30 @@ namespace TMG.FlowField
 			CreateGrid();
 		}
 
+		private void Update()
+		{
+			if((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetMouseButtonDown(0))
+			{
+				//Debug.Log("Ctrl + Click");
+				Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+				Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(mousePos);
+				Node unwalkNode = GetNodeFromWorldPos(worldMousePos);
+				unwalkNode.MakeImpassible();
+			}
+
+			else if ((Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)) && Input.GetMouseButtonDown(0))
+			{
+				//Debug.Log("Alt + Click");
+
+			}
+
+			else if (Input.GetMouseButtonDown(0))
+			{
+				//Debug.Log("Click");
+				//Set goal & recalc cost
+			}
+		}
+
 		private void CreateGrid()
 		{
 			Debug.Log("Creating Cube");
@@ -28,7 +52,7 @@ namespace TMG.FlowField
 			{
 				for(int y = 0; y < gridSize.y; y++)
 				{
-					Vector3 worldPos = new Vector3(nodeDiameter * x, 0, nodeDiameter * y);
+					Vector3 worldPos = new Vector3(nodeDiameter * x + nodeRadius, 0, nodeDiameter * y + nodeRadius);
 					grid[x, y] = new Node(true, worldPos);
 				}
 			}
@@ -46,6 +70,19 @@ namespace TMG.FlowField
 					Gizmos.DrawCube(n.worldPos, Vector3.one * (nodeDiameter - 0.1f));
 				}
 			}
+		}
+
+		Node GetNodeFromWorldPos(Vector3 worldPos)
+		{
+			float percentX = worldPos.x / (gridSize.x * nodeDiameter);
+			float percentY = worldPos.z / (gridSize.y * nodeDiameter);
+			Debug.Log("X: " + percentX + " Y: " + percentY);
+			percentX = Mathf.Clamp01(percentX);
+			percentY = Mathf.Clamp01(percentY);
+			
+			int x = Mathf.FloorToInt((gridSize.x) * percentX);
+			int y = Mathf.FloorToInt((gridSize.y) * percentY);
+			return grid[x, y];
 		}
 	}
 }
--- FlowField/Assets/Scripts/Node.cs
+++ FlowField/Assets/Scripts/Node.cs
@@ -4,10 +4,27 @@ public class Node
 {
 	public bool walkable;
 	public Vector3 worldPos;
+	public byte cost;
+	public ushort bestCost;
 	
 	public Node(bool _walkable, Vector3 _worldPos)
 	{
 		walkable = _walkable;
 		worldPos = _worldPos;
+		cost = 1;
+		bestCost = ushort.MaxValue;
+	}
+
+	public void IncreaseCost()
+	{
+		if(cost == byte.MaxValue) { return; }
+		cost++;
+		if(cost == byte.MaxValue) { walkable = false; }
+	}
+
+	public void MakeImpassible()
+	{
+		cost = byte.MaxValue;
+		walkable = false;
 	}
 }
