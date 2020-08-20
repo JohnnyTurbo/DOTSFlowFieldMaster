441114f9e3c3746496d136e2c2407ad561263883
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2020-08-20 11:28:58 -0700

    Began adding in perlin noise functionality for cost field.

---
+++ FlowField/Assets/Scripts/ECS/GeneratePerlinNoiseSystem.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Entities;
+
+namespace TMG.ECSFlowField
+{
+    public class GeneratePerlinNoiseSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            Entities.ForEach((in GeneratePerlinNoiseTag noiseTag) =>
+            {
+                
+            }).Run();
+        }
+    }
+}
