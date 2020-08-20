f869c82bc8d15a38002afc575c95ec87e71163c6
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2020-08-20 11:29:03 -0700

    Began adding in perlin noise functionality for cost field.

---
+++ FlowField/Assets/Scripts/ECS/PerlinNoiseData.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace TMG.ECSFlowField
+{
+    public struct PerlinNoiseData : IComponentData
+    {
+        public float2 perlinTextureSize;
+        public bool randomizeNoiseOffset;
+        public float2 perlinOffset;
+        public float noiseScale;
+        public int2 perlinGridStepSize;
+    }
+}
