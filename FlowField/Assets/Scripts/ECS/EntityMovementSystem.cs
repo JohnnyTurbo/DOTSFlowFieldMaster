762537cb58a09a21a60bd33ba292e14775ce6d25
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2020-08-27 07:42:35 -0700

    Implemented multithreading for Entity Movement.

--- FlowField/Assets/Scripts/ECS/EntityMovementSystem.cs
+++ FlowField/Assets/Scripts/ECS/EntityMovementSystem.cs
@@ -18,11 +18,13 @@ namespace TMG.ECSFlowField
         private DestinationCellData _destinationCellData;
         private DynamicBuffer<EntityBufferElement> _entityBuffer;
         private DynamicBuffer<Entity> _gridEntities;
+        private EntityCommandBufferSystem _ecbSystem;
         private static NativeArray<CellData> _cellDatas;
         
         protected override void OnCreate()
         {
             instance = this;
+            _ecbSystem = World.GetOrCreateSystem<EntityCommandBufferSystem>();
         }
 
         public void SetSingleton()
@@ -49,15 +51,18 @@ namespace TMG.ECSFlowField
         protected override void OnUpdate()
         {
             if (_flowFieldEntity.Equals(Entity.Null)) {return;}
+            var commandBuffer = _ecbSystem.CreateCommandBuffer().AsParallelWriter();
             float deltaTime = Time.DeltaTime;
             FlowFieldData flowFieldData = _flowFieldData;
             int2 destinationCell = _destinationCellData.destinationIndex;
             //NativeArray<CellData> cellDatas = new NativeArray<CellData>(_cellDatas.Length, Allocator.TempJob);
             //cellDatas = _cellDatas;
             JobHandle jobHandle = new JobHandle();
-            /*jobHandle = */Entities.ForEach((ref PhysicsVelocity physVelocity, ref EntityMovementData entityMovementData,
-                ref Translation translation) =>
+            jobHandle = Entities.ForEach((Entity entity, int entityInQueryIndex, ref PhysicsVelocity physVelocity, 
+                ref EntityMovementData entityMovementData, ref Translation translation) =>
             {
+                //PhysicsVelocity newPhysicsVelocity = physVelocity;
+                //Translation newTranslation = translation;
                 int2 curCellIndex = ECSHelper.GetCellIndexFromWorldPos(translation.Value, flowFieldData.gridSize,
                     flowFieldData.cellRadius * 2);
                 if (curCellIndex.Equals(destinationCell))
@@ -67,6 +72,7 @@ namespace TMG.ECSFlowField
                 int flatCurCellIndex = ECSHelper.ToFlatIndex(curCellIndex, flowFieldData.gridSize.y);
                 float2 moveDirection = _cellDatas[flatCurCellIndex].bestDirection;
                 float finalMoveSpeed = (entityMovementData.destinationReached ? 1f : entityMovementData.moveSpeed) * deltaTime;
+                
                 physVelocity.Linear = new float3
                 {
                     x = moveDirection.x * finalMoveSpeed,
@@ -74,10 +80,12 @@ namespace TMG.ECSFlowField
                     z = moveDirection.y * finalMoveSpeed
                 };
                 translation.Value.y = 0f;
+                //commandBuffer.SetComponent(entityInQueryIndex, entity, newPhysicsVelocity);
+                //commandBuffer.SetComponent(entityInQueryIndex, entity, newTranslation);
                 //physVelocity.Linear.xz = moveDirection * finalMoveSpeed;
 
-            }).Run();//ScheduleParallel(jobHandle);
-            //jobHandle.Complete();
+            })./*Run();*/ScheduleParallel(jobHandle);
+            jobHandle.Complete();
             //cellDatas.Dispose();
         }
     }
